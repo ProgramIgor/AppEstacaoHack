5003b6b567ec9a058324a2bfcf02aa740982ba77
Igor Soares <programigor12@gmail.com>
2021-11-25 22:29:50 -0300

    Iniciando a adição do metodo BINDING

--- app/src/main/java/com/example/igor/MainActivity.kt
+++ app/src/main/java/com/example/igor/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.igor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.igor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMainSair.
     }
 }
